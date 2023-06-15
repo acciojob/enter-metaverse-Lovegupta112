dd5d2a8ddae037a16695020fe80cdfbad70abf91
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-06-15 18:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+window.addEventListener('load',()=>{
+	let div=document.createElement('div');
+	let p=document.createElement('p');
+	p.className='status';
+	p.innerText="Enter the Metaverse";
+
+	div.append(p);
+	
+	let btn=document.createElement('Button');
+	btn.id='enterBtn';
+	btn.innerText='Enter';
+	btn.addEventListener('click',change);
+	body.append(div);
+	body.append(btn);
+})
+
+function change(){
+	let div.document.getElementsByTagName('div')[0];
+	
+	let h1=document.createElement('h1');
+	h1.className='status';
+	h1.innerText='Entered Metaverse';
+
+	div.innerHTML="";
+	div.append(h1);
+}
+
+
+
+
+
+
+
+
+
+
